ba75d7722d614aa25df219bd46439a3d67c9365b
Bowen Song <sbowen@bu.edu>
2017-11-06 19:50:04 -0500

    e2 done

--- README.md
+++ README.md
@@ -6,3 +6,11 @@ A program reads the cvMat object from rows to column to type. We can specify the
 
 Exercise 2
 
+1. The outputs are the intensity of the specified value. For example blue has the intensity of the blue in each pixel and displayed as gray scale. Since there is only one element of the RGB is distracted, the intensity value can only differentiate from white and black. White being high value. 
+
+2. For the Lena image, the values at [20 25]:
+('For RGB ', '[106 122 225]')
+('For YCbCr ', '[151 181 103]')
+('For HSV ', '[  4 135 225]')
+
+I also found the splits, while not assigned to the photo, is still assigning values.
--- pyColorImage.py
+++ pyColorImage.py
@@ -3,30 +3,51 @@ import io
 
 # this was fun -- Bowen
 
+x = 20
+y = 25
+
+def showandwrite(filename, img):
+	cv2.imshow(filename, img)
+	cv2.imwrite(''.join([filename,".png"]),img)
+
+
 
 src = cv2.imread("Test_images/Lenna.png", 1)
-cv2.namedWindow( 'Original image', cv2.WINDOW_AUTOSIZE );
-cv2.imshow( "Original image", src);
-
-
-R,G,B = cv2.split(src);
-cv2.imshow("Red",R);
-cv2.imshow("Green",G);
-cv2.imshow("Blue",B);
+print("For RGB ",str(src[x,y]))
+#cv2.namedWindow( 'Original image', cv2.WINDOW_AUTOSIZE );
+#cv2.imshow( "Original image", src);
+showandwrite( "Original image", src)
 
 ycrcb_image = cv2.cvtColor(src, cv2.COLOR_BGR2YCR_CB);
-y,cb,cr = cv2.split(ycrcb_image)
-cv2.imshow("Y",y);
-cv2.imshow("Cb",cb);
-cv2.imshow("Cr",cr);
+print("For YCbCr ",str(ycrcb_image[x,y]))
 
 HSV_image = cv2.cvtColor(src, cv2.COLOR_BGR2HSV);
+print("For HSV ",str(HSV_image[x,y]))
+
+R,G,B = cv2.split(src);
+showandwrite("Red",R);
+showandwrite("Green",G);
+showandwrite("Blue",B);
+
+
+y,cb,cr = cv2.split(ycrcb_image)
+showandwrite("Y",y);
+showandwrite("Cb",cb);
+showandwrite("Cr",cr);
+
+
 h,s,v = cv2.split(HSV_image)
-cv2.imshow("Hue",  h);
-cv2.imshow("Saturation", s);
-cv2.imshow("Value", v);
+showandwrite("Hue",  h);
+showandwrite("Saturation", s);
+showandwrite("Value", v);
+
+#print("The RGB values are: Blue " + src[x,y,0] +" Green "+ src[x,y,1] +" red "+ src[x,y,2] )
 
 while (1):
 	k = cv2.waitKey(33)
 	if k==27:
 		break
+
+
+
+
